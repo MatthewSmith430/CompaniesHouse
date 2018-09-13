05d81cc3ea5ae765235127c56e8e438f18e67e92
Matthew Smith <matt_smith.430@tiscali.co.uk>
2018-09-13 16:24:00 +0100

    update_sept13

---
+++ R/CompanyDataExtract_function.R
@@ -0,0 +1,79 @@
+#' @title Company Data Extraction
+#'
+#' @description This function extracts company information based on a company number
+#' @param company_number Company number
+#' @param mkey Authorisation key
+#' @export
+#' @return Dataframe listing company name, company number, postcode, SIC code for the company number
+CompanyDataExtract <- function(company_number,mkey) {
+  company_number<-as.character(company_number)
+  FIRMurl<-paste0("https://api.companieshouse.gov.uk/company/",company_number)
+  firmTEST<-httr::GET(FIRMurl, httr::authenticate(mkey, ""))
+  firmTEXT<-httr::content(firmTEST, as="text")
+  JLfirm<-jsonlite::fromJSON(firmTEXT, flatten=TRUE)
+
+  if (length(JLfirm)<15){
+    DFfirm<-plyr::ldply(JLfirm,data.frame)
+
+    DFfirm2<-t(DFfirm)
+    DFfirm2<-as.data.frame(DFfirm2)
+    R<-DFfirm2[1,]
+    R<-unlist(R)
+    R<-as.character(R)
+    colnames(DFfirm2)<-R
+    DFfirm2<-DFfirm2[-1,]
+    K<-DFfirm2[1,]
+    DFfirmNAMES<-empty2na(as.character(K$company_name))
+    DFfirmSIC<-"NA"
+    DFfirmNUMBER<-empty2na(as.character(K$company_number))
+    DFfirmDateofCreation<-"NA"
+    DFfirmTYPE<-"NA"#empty2na(as.character(DFfirm$type))
+    DFfirmSTATUS<-"NA"#empty2na(as.character(DFfirm$company_status))
+    DFfirmADDRESS<-"NA"#empty2na(as.character(DFfirm$registered_office_address.address_line_1))
+    DFfirmLOCAL<-"NA"#empty2na(as.character(DFfirm$registered_office_address.locality))
+    DFfirmREGION<-"NA"#empty2na(as.character(DFfirm$registered_office_address.region))
+    DFfirmPOSTCODE<-"NA"#empty2na(as.character(DFfirm$registered_office_address.postal_code))
+
+    myDf <- data.frame(
+      company.number = DFfirmNUMBER,
+      company.name=DFfirmNAMES,
+      sic.code=DFfirmSIC,
+      Date.of.Creation = DFfirmDateofCreation,
+      company.type = DFfirmTYPE,
+      company.status = DFfirmSTATUS,
+      address = DFfirmADDRESS,
+      locality = DFfirmLOCAL,
+      region = DFfirmREGION,
+      postcode = DFfirmPOSTCODE)
+  }else{
+    DFfirm<-data.frame(JLfirm)
+
+    DFfirmNAMES<-as.character(DFfirm$company_name)
+    DFfirmSIC<-empty2na(as.character(DFfirm$sic_codes))
+    DFfirmNUMBER<-empty2na(as.character(DFfirm$company_number))
+    DFfirmDateofCreation<-empty2na(as.character(DFfirm$date_of_creation))
+    DFfirmTYPE<-empty2na(as.character(DFfirm$type))
+    DFfirmSTATUS<-empty2na(as.character(DFfirm$company_status))
+    DFfirmADDRESS<-empty2na(as.character(DFfirm$registered_office_address.address_line_1))
+    DFfirmLOCAL<-empty2na(as.character(DFfirm$registered_office_address.locality))
+    DFfirmREGION<-empty2na(as.character(DFfirm$registered_office_address.region))
+    DFfirmPOSTCODE<-empty2na(as.character(DFfirm$registered_office_address.postal_code))
+
+    myDf <- data.frame(
+      company.number = DFfirmNUMBER,
+      company.name=DFfirmNAMES,
+      sic.code=DFfirmSIC,
+      Date.of.Creation = DFfirmDateofCreation,
+      company.type = DFfirmTYPE,
+      company.status = DFfirmSTATUS,
+      address = DFfirmADDRESS,
+      locality = DFfirmLOCAL,
+      region = DFfirmREGION,
+      postcode = DFfirmPOSTCODE)
+  }
+  #plyr::
+
+
+
+  return(myDf)
+}
---
+++ R/empty2na_function.R
@@ -0,0 +1,15 @@
+#' @title empty2na
+#'
+#' @description Check is an object is empty and return "NA if it is.
+#' @param x data
+#' @export
+#' @return NA if empty, data otherwise
+#'
+empty2na<-function(x){
+  CHECKres<-installr::is.empty(x)
+  if (CHECKres==TRUE){
+    d<-"NA"
+  }else{d<-x}
+  return(d)
+}
+
